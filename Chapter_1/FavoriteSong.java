f6535f0c719973ddf849793964a22d86f666a1ac
AdmerOlbedo-UCM <eigikyut@gmail.com>
2025-09-02 13:47:11 -0300

    Create FavoriteSong.java

---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,8 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        System.out.println("Nandito ako, umiibig sa 'yo");
+        System.out.println("Kahit na nagdurugo ang puso");
+        System.out.println("Kung sakaling iwanan ka niya");
+        System.out.println("Huwag kang mag-alala, may nagmamahal pa rin sa 'yo");
+    }
+}
