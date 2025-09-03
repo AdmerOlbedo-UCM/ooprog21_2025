744dfdebb16f66cb0cfaf325792a1d20563f9a16
AdmerOlbedo-UCM <eigikyut@gmail.com>
2025-09-03 12:24:40 -0300

    Create Comments.java

---
+++ Chapter_1/Comments.java
@@ -0,0 +1,8 @@
+public class Comments {
+    public static void main(String[] args) {
+        
+        System.out.println("Hello! This program demonstrates different comments in Java.");
+        
+        System.out.println("This line is explained using a multi-line comment.");
+    }
+}
