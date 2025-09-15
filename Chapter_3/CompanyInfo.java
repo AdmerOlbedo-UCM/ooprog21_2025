be56e985f6faf4031b01729b966d7f59b23a5505
AdmerOlbedo-UCM <eigikyut@gmail.com>
2025-09-15 12:38:32 -0300

    Create CompanyInfo.java

---
+++ Chapter_3/CompanyInfo.java
@@ -0,0 +1,9 @@
+public class CompanyInfo {
+    public static void main(String[] args) {
+        System.out.println("TechNova Solutions\n" +
+                         "Smart Electronics\n" +
+                         "Monday - Friday: 8 am to 6 pm\n" +
+                         "Saturday: 8 am to noon\n" +
+                         "Sunday: Closed");
+    }
+}
